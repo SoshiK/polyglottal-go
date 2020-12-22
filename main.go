aa64699e375f9b9298dd639ffaed23e15ff250e3
Soshi <SoshiK@users.noreply.github.com>
2020-12-22 11:58:00 +0900

    /api/listsと/api/items/:listIdの追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"os"
@@ -13,12 +14,27 @@ import (
 	"gorm.io/gorm"
 )
 
+type List struct {
+	gorm.Model
+	Title       string
+	Description string
+	Items       []Item
+}
+
+type Item struct {
+	gorm.Model
+	Title       string
+	Description string
+	Url         string
+	ListID      uint
+}
+
 func main() {
 	//ecnoインスタンスの生成 -> express的な
 	e := echo.New()
 
 	//ミドルウェア関数の登録
-	e.Use(middleware.Logger())
+	// e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	//環境変数の読み込み
@@ -41,6 +57,23 @@ func main() {
 	//GET - /
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	//GET - /api/lists
+	e.GET("/api/lists", func(c echo.Context) error {
+		users := []List{}
+		db.Find(&users)
+		res, _ := json.Marshal(users)
+		return c.JSONBlob(http.StatusOK, res)
+	})
+
+	//GET - /api/items/:listId
+	e.GET("/api/items/:listId", func(c echo.Context) error {
+		listId := c.Param("listId")
+		items := []Item{}
+		db.Where("list_id = ?", listId).Find(&items)
+		res, _ := json.Marshal(items)
+		return c.JSONBlob(http.StatusOK, res)
 	})
 	//port3000でサーバーをたてる
 	e.Logger.Fatal(e.Start(":3000"))
